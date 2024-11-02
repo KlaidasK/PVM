93499fcaf47393a794bae3e4b2cce2d147c8aedd
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2024-11-02 23:21:22 +0200

    Pridėtas tuščias user app

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+
+# cia itraukiam view funkcijas per view.funkcijos_pav. Galit kaip pavyzdi 
+# ziuret frontend app failus.
+urlpatterns = [
+    
+]
