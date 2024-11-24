069b0440e8eb832a379da29f75f6d0f5cbdd854b
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2024-11-24 15:13:18 +0200

    Atnaujintas profile view šablonas

--- user/models.py
+++ user/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    country = models.CharField(max_length=20)
+    gender = models.CharField(max_length=10)
+    language = models.CharField(max_length=20)
+    dob = models.DateField()
     bio = models.CharField(max_length=500)
     
     def __str__(self):
